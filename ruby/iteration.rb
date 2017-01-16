6cc67ad289d0ee3ef41d8a334c9e4127e7bd9ada
Jeremy Cvicker <jcvicker@gmail.com>
2017-01-16 00:45:41 -0600

    further complete lessons, fill in with methods

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -46,7 +46,30 @@ end
 p upcase_states
 
   
-  
-  
-  
+number_array = [237, 23, 690, 51, 875, 111]
+
+def low_discarder(numbers)
+	numbers.delete_if {|value| value <= 110}
+end
+
+p low_discarder(number_array)
+
+def num_keeper(numbers)
+	numbers.keep_if {|value| value.odd?  }
+end
+
+p num_keeper(number_array)
+
+def odd_numbers(numbers)
+	numbers.select {|value| value.odd?}
+end
+
+p odd_numbers(number_array)
+
+
+def reject_number(numbers)
+	numbers.drop_while {|value| numbers.length <= 3 }
+end
+
+p reject_number(number_array)
   
