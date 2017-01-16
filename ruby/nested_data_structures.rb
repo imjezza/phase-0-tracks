e02d9969ded7b8d16f43691ca785af773fac0778
Jeremy Cvicker <jcvicker@gmail.com>
2017-01-16 10:47:27 -0600

    create nested array, start to ask questions

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,22 @@
+office_building = {
+	bosses_office: {
+	  large_desk: 'pen drawer',
+	  decoration: {fern: 'green', photo: 'family'}, 
+	  window: 'great view',
+	  },
+ 
+
+  my_office: {
+    desk: 'cubicle',
+    decoration: 'photo',
+    window: 'none'
+  },
+  
+  bathroom: {
+    sink: 'water',
+    stall: ['toilet', 'coat hook']
+  }
+
+}
+
+office_building[bosses_office:][large_desk:]
