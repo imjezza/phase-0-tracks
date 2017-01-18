803713a4048ef4555216b3cad7cc1e50e99fdb83
Jeremy Cvicker <jcvicker@gmail.com>
2017-01-17 21:42:31 -0600

    complete release

---
+++ ruby/puppy.rb
@@ -0,0 +1,11 @@
+species -----------------
+puppius cutus
+
+characterisitcs ----------
+Fur feeling : soft as heck
+Friently : oh yea
+Eye count : two thank goodness
+
+BEHAVIOUR -----------------
+Good boy : yes
+Kisses : yes
