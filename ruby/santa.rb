43619aa7b9f8d168eb54bb8aa1530922e449c359
Jeremy Cvicker <jcvicker@gmail.com>
2017-01-20 18:59:21 -0600

    more addtion to code

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,7 +1,12 @@
 class Santa
 
-	def initialize
+	def initialize(gender, ethnicity)
 		p "initialize Santa instance...."
+		@gender = gender
+   		@ethnicity = ethnicity
+   		@reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+   		@age = 0
+
 	end
 
 	def speak(repetitions)
@@ -11,9 +16,27 @@ class Santa
 	def eat_milk_and_cookies(cookie_type)
 		p "That was a good #{cookie_type} cookie!"
 	end
+
+	def celebrate_age
+		@age += 1
+	end
+
+	def get_mad_at(rude_reindeer)
+		@reindeer_ranking.insert(@reindeer_ranking.length.to_i - 1, @reindeer_ranking.delete_at(@reindeer_ranking.index(rude_reindeer)))
+	end
+
 end
+
+
+
+
+
 
 santa_man = Santa.new
 
 santa_man.speak(2)
 santa_man.eat_milk_and_cookies("chocolate chip")
+
+p santa.age
+p santa.ethnicity
+p santa.gender
