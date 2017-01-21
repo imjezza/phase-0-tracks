0f37da2546858ca13d6d84c349b841c2b6e5e074
Jeremy Cvicker <jcvicker@gmail.com>
2017-01-20 21:02:27 -0600

    create and complete flight release, then update shout driver code with mixins and classes

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,15 +1,33 @@
 #modules, both happy and angry
 
 module Shout
- def self.yell_angrily(words)
-    words + "!!!" + " :("
+ def yell_angrily(words)
+    p words + "!!!" + " :("
  end
 
- def self.yelling_happily(words)
-    words + "!!!" + " :P"
+ def yelling_happily(words)
+   p words + "!!!" + " :P"
  end
 end
 
 # Driver Code
-puts Shout.yelling_happily("Its saturday, and I love the weekend")
-puts Shout.yell_angrily("Its gonna be a rough four years of the trump administration")
+#puts Shout.yelling_happily("Its saturday, and I love the weekend")
+#puts Shout.yell_angrily("Its gonna be a rough four years of the trump administration")
+
+# some classes
+
+class Wife
+	include Shout
+end
+
+class Child
+	include Shout
+end
+
+# New Driver Codes
+
+my_wife = Wife.new
+my_wife.yell_angrily("How could you forget our anniversary oh em gee")
+
+my_son = Child.new
+my_son.yelling_happily("I looove ice cream and treats")
