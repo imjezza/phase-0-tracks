6dd25763701f40f8ef51c0797f47b66cdb84031c
Jeremy Cvicker <jcvicker@gmail.com>
2017-02-07 22:14:08 -0600

    add comments

---
+++ ruby/puppy_frogs.rb
@@ -0,0 +1,14 @@
+# SPECIES ------------------------
+# Dogus Barkus
+
+# CHARACTERISTICS ----------------
+# Fur: Soft
+# Friendly: yes
+# Ears: Floppy
+# Name: varies
+# Breed: varies
+
+# BEHAVIOR -----------------------
+# Bark
+# Roll Over
+# Fetch
