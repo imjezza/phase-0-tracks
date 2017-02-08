ebb5842af80810ccb4efac286846e44d079c9b1b
Jeremy Cvicker <jcvicker@gmail.com>
2017-02-07 22:48:06 -0600

    create car class, add methods, and driver code to test they work

--- ruby/puppy_frog_methods.rb
+++ ruby/puppy_frog_methods.rb
@@ -1,26 +1,60 @@
 class Puppy
 
-  def fetch(toy)
-    puts "I brought back the #{toy}!"
-    toy
-  end
+	def initialize
+		puts "Initializing new puppy instance..."
+	end
 
-  def speak(quantity)
-  	quantity.times {puts "Woof!"}
-  end
+	def fetch(toy)
+    	puts "I brought back the #{toy}!"
+    	toy
+  	end
 
-  def roll_over
-  	puts "*rolls over*"
-  end
+  	def speak(quantity)
+  		quantity.times {puts "Woof!"}
+  	end
 
-  def dog_years(years)
-  	puts years * 7
-  end
+  	def roll_over
+  		puts "*rolls over*"
+  	end
 
-  def chases_tail(quantity)
-  	quantity.times {puts "chases tail"}
-  end
+  	def dog_years(years)
+  		puts years * 7
+  	end
+
+  	def chases_tail(quantity)
+  		quantity.times {puts "chases tail"}
+  	end
 end
+
+
+
+class Car
+	def initialize
+		puts "Initializing new car class..."
+	end
+
+	def starter
+		puts "*turns key*"
+		puts "engine rumbles"
+	end
+
+	def gas_pedal(interger)
+		if interger == 0
+			puts "we're not getting anywhere soon..."
+		elsif interger == 1
+			puts "now we're getting somewhere"
+		elsif interger == 2
+			puts "woah baby now thats some speed!"
+		elsif interger >= 3
+			puts "TOO FAST TOO FAST"
+		end
+	end
+
+	def horn(interger)
+		interger.times {puts "Beep!"}
+	end
+end
+
 
 
 #driver code
@@ -33,4 +67,7 @@ rex.dog_years(3)
 rex.chases_tail(2)
 
 
-
+dodge = Car.new
+dodge.starter
+dodge.gas_pedal(1)
+dodge.horn(2)
