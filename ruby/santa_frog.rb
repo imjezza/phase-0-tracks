a71d2f83e95291a27808861a4b16beb8b43987f3
Jeremy Cvicker <jcvicker@gmail.com>
2017-02-11 22:49:13 -0600

    complete code for lesson, creating 15000 instances of santas with driver code added

--- ruby/santa_frog.rb
+++ ruby/santa_frog.rb
@@ -1,6 +1,14 @@
 class Santa
-	def initialize 
+
+	attr_reader :age, :ethnicity
+	attr_accessor :gender
+
+	def initialize(gender, ethnicity) 
 		puts "initializing Santa instance..."
+		@gender = gender
+		@ethnicity = ethnicity
+		@reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+		@age = 0
 	end
 
 	def speak
@@ -10,8 +18,23 @@ class Santa
 	def eat_milk_and_cookies(cookie)
 		puts "that was a good #{cookie} cookie!"
 	end
+
+	def celebrate_birthday
+		@age =+ 1
+	end
+	
+  def get_mad_at(bad_reindeer)
+    @raindeer_ranking.insert(@raindeer_ranking.length.to_i - 1, @raindeer_ranking.delete_at(@raindeer_ranking.index(bad_reindeer)))
+  end
 end
 
-brian = Santa.new
-brian.speak
-brian.eat_milk_and_cookies("chocolate chip")
+example_genders = ["very tall", "both", "all of them", "male", "female", "muscular"]
+example_ethnicities = ["Latino", "White", "Yemeni", "Martian", "Zombie"]
+
+15000.times do
+  santa = Santa.new(example_genders[rand(example_genders.length.to_i - 1)], example_ethnicities[rand(example_ethnicities.length.to_i - 1)])
+  rand(140).times {santa.celebrate_birthday}
+  p santa.age
+  p santa.ethnicity
+  p santa.gender
+end
