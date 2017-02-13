8cc043e7845649cd6caba41b38003fd9635341b9
Jeremy Cvicker <jcvicker@gmail.com>
2017-02-13 00:32:18 -0600

    add methods to pass tests provided

--- ruby/list/frog_todo_list.rb
+++ ruby/list/frog_todo_list.rb
@@ -1,3 +1,19 @@
 class Todolist
 
+	def initialize(list)
+		@list = list
+	end
+
+	def get_items
+		@list
+	end
+
+	def add_item(item)
+		@list << item
+	end
+
+	def delete_item(item)
+		@list.delete(item)
+	end
+
 end
